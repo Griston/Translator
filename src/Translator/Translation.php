b037eb19cd62f1e82c8a6d43653c9915969841db
David Skála <david.skala@winternet.cz>
2018-09-13 10:21:52 +0200

    first try of own package...

---
+++ src/Translator/Translation.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Griston\Translator;
+
+use Nette\SmartObject;
+
+class Translation
+{
+
+    /** @var \Nette\Localization\ITranslator */
+    private $translator;
+
+    /** @var string */
+    private $text = '';
+
+    /** @var array */
+    private $params = [];
+
+    /**
+     * Translation constructor.
+     * @param \Nette\Localization\ITranslator $translator
+     * @param $text
+     * @param array $params
+     */
+    function __construct(\Nette\Localization\ITranslator $translator, $text, array $params)
+    {
+        $this->translator = $translator;
+        $this->text = $text;
+        $this->params = $params;
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param array $params
+     * @return $this
+     */
+    function setParams(array $params)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    function getParams()
+    {
+        return $this->params;
+    }
+
+    /**
+     * @param $param
+     * @return $this
+     */
+    function addParam($param)
+    {
+        $this->params[] = $param;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return vsprintf($this->translator->translate($this->text), $this->params);
+    }
+
+}
---
+++ src/Translator/Translator.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Griston\Translator;
+
+use App\Model\Translation\TranslationFacade;
+use Nette\Caching;
+use Nette\Http\Session;
+use Nette\Localization\ITranslator;
+
+/**
+ * Description of Translator
+ *
+ * @author David Skála <skala2524@gmail.com>
+ */
+class Translator implements ITranslator
+{
+
+    CONST TMP_LANG = [
+        'cs' => 1
+    ];
+
+    /** @var boolean */
+    private $isCacheInvalidated = false; // zatím není přidáno...
+
+    /** @var TranslationFacade */
+    private $facadeTranslation;
+
+    /** @var Caching\Cache */
+    private $cache;
+
+    /** @var integer */
+    private $languageId;
+
+    /** @var array */
+    private $traslations;
+
+    /** @var Session */
+    public $session;
+
+    /**
+     * @return int
+     */
+    public function getLanguageId()
+    {
+        return $this->languageId;
+    }
+
+    /**
+     * Translator constructor.
+     * @param TranslationFacade $facadeTranslation
+     * @param Caching\IStorage $cacheStorage
+     * @param Session $session
+     */
+    public function __construct(TranslationFacade $facadeTranslation, Caching\IStorage $cacheStorage, Session $session)
+    {
+        $this->facadeTranslation = $facadeTranslation;
+        $this->cache = new Caching\Cache($cacheStorage, sprintf('Translator.Translation'));
+        $this->session = $session;
+    }
+
+    /**
+     * @param $languageId
+     * @return Translator
+     */
+    public function setLanguage($languageId): Translator
+    {
+        $this->languageId = self::TMP_LANG[$languageId];
+        $this->loadTranslations();
+//$this->invalidateCache();
+        return $this;
+    }
+
+    /**
+     * @param $message
+     * @param null $count
+     * @return null|string
+     */
+    public function translate($message, $count = null)
+    {
+        if (is_null($this->traslations))
+        {
+            return null;
+        }
+
+        if ($message instanceof Translation)
+        {
+            return $message;
+        }
+
+        $_message = trim($message);
+        if ($_message)
+        {
+            $hash = md5($_message);
+            if (!isset($this->traslations[$hash]))
+            {
+                // \Tracy\Debugger::barDump(sprintf('translate:isNotIn:%s', $_message));
+                try
+                {
+                    $this->facadeTranslation->addNewTranslation($_message);
+                } catch (\Exception $ex)
+                {
+                    // překlad již existuje, ale není v cachi, kterou budeme stejně invalidovat...
+                }
+                $this->invalidateCache();
+            }
+            $_message = $this->getCorrectFormat($this->traslations[$hash], abs($count));
+            $this->session->getSection('g-translator')->translations[$hash] = $_message;
+        }
+
+        return $_message;
+    }
+
+    /**
+     * @param \App\Model\Translation\TranslationMutation $mutation
+     * @param $count
+     * @return string
+     */
+    private function getCorrectFormat(\App\Model\Translation\TranslationMutation $mutation, $count)
+    {
+        if (in_array($count, [
+            null,
+            1
+        ]))
+        {
+            return $mutation->getSingular() ?: '';
+        }
+        elseif (in_array($count, [
+            2,
+            3,
+            4
+        ]))
+        {
+            return $mutation->getPlural1() ?: '';
+        }
+        else
+        {
+            return $mutation->getPlural2() ?: '';
+        }
+    }
+
+    /**
+     * @param null $languageId
+     */
+    public function invalidateCache($languageId = null)
+    {
+        $_languageId = is_null($languageId) ? $this->languageId : $languageId;
+        $this->cache->remove($_languageId);
+
+        if ($_languageId == $this->languageId)
+        {
+            $this->loadTranslations();
+        }
+    }
+
+    /**
+     *
+     */
+    private function loadTranslations()
+    {
+        $this->traslations = $this->cache->load($this->languageId, function ()
+        {
+            $mutations = array_reduce($this->facadeTranslation->getMutationsByLanguage($this->languageId), function ($result, \App\Model\Translation\TranslationMutation $mutation)
+            {
+                $result[$mutation->getTranslation()->getHash()] = $mutation;
+                return $result;
+            }, []);
+            return $mutations;
+        });
+    }
+
+    /**
+     * @param string $text
+     * @param array $params
+     * @return Translation
+     */
+    public function getTranslation($text = '', array $params = [])
+    {
+        $translation = new Translation($this, $text, $params);
+        return $translation;
+    }
+
+}
